fda6c24a176853b9f0c92f15b22044b33edb6c71
Autumn <autumncbullard@gmail.com>
2018-02-15 12:05:19 -0500

    Build function that selects each number

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var result = '0';
+
+// FUNCTIONS
+
+function selectNumber(elem) {
+  result = result.split(' ');
+
+  if (result[result.length - 1] === '0') {
+    result[result.length - 1] = result[result.length - 1].substr(1);
+  }
+  result[result.length - 1] += elem.id;
+  document.getElementById('screen').value = result[result.length - 1];
+  result = result.join(' ');
+  console.log(result);
+}
